9b1d7ae1dd55a4c71fcce7614dddad1ff2f5d6fb
kizekzoltan <kizekzoltan@gmail.com>
2021-05-09 17:50:47 +0200

    mértékegység javítások, kell uppercase

--- javasrcipt.js
+++ javasrcipt.js
@@ -1,6 +1,6 @@
 function szamolasNegyzet() {  
     var aOldal = document.getElementById("oldal").value;
-    var negyzetTerulet = "A területe " + aOldal * aOldal + " cm.";
+    var negyzetTerulet = "A területe " + aOldal * aOldal + " cm2.";
     document.getElementById("terulet").innerHTML = negyzetTerulet;
     var negyzetKerulet = "A kerülete " + aOldal * 4 + " cm.";
     document.getElementById("kerulet").innerHTML = negyzetKerulet;
@@ -18,7 +18,7 @@ function szamolasKor(){
     var sugar = document.getElementById("sugar").value;
     var pi = 3.14
     
-    var korTerulet = "A területe " + (sugar*sugar) * pi + " cm.";
+    var korTerulet = "A területe " + (sugar*sugar) * pi + " cm2.";
     document.getElementById("terulet").innerHTML = korTerulet;
 
     var korKerulet = "A kerülete " + (2 * sugar) * pi + " cm";
@@ -27,9 +27,9 @@ function szamolasKor(){
 
 function szamolasKocka(){
     var aOldal = document.getElementById("oldal").value;
-    var kockaFelszin = "A kocka felszíne " + (aOldal*aOldal) * 6 + " cm."; 
+    var kockaFelszin = "A kocka felszíne " + (aOldal*aOldal) * 6 + " cm2."; 
     document.getElementById("felszin").innerHTML = kockaFelszin;
-    var kockaTerfogat = "A kocka felszíne " + aOldal*aOldal*aOldal + " cm.";
+    var kockaTerfogat = "A kocka térfogata " + aOldal*aOldal*aOldal + " cm3.";
     document.getElementById("terfogat").innerHTML = kockaTerfogat;
   }
 
@@ -37,8 +37,8 @@ function szamolasTeglatest(){
     var aOldal = document.getElementById("a").value;
     var bOldal = document.getElementById("b").value;
     var cOldal = document.getElementById("c").value;
-    var teglatestFelszin = "A téglatest felszíne " + 2 * (aOldal*bOldal+aOldal*cOldal+bOldal*cOldal) + " cm.";
+    var teglatestFelszin = "A téglatest felszíne " + 2 * (aOldal*bOldal+aOldal*cOldal+bOldal*cOldal) + " cm2.";
     document.getElementById("felszin").innerHTML = teglatestFelszin;
-    var teglatestTerfogat = "A téglatest térfogata " + aOldal*bOldal*cOldal + " cm.";
+    var teglatestTerfogat = "A téglatest térfogata " + aOldal*bOldal*cOldal + " cm3.";
     document.getElementById("terfogat").innerHTML = teglatestTerfogat;
 }
